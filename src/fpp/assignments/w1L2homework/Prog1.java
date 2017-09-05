b92374ed32ddb1ff736e5f5ca01d8ad9d9c213dc
geetaputtappanavar <31633888+geetaputtappanavar@users.noreply.github.com>
2017-09-04 21:28:16 -0500

    Made changes

--- src/fpp/assignments/w1L2homework/Prog1.java
+++ src/fpp/assignments/w1L2homework/Prog1.java
@@ -3,10 +3,9 @@ package fpp.assignments.w1L2homework;
 public class Prog1 {
 
 	public static void main(String[] args) {
-		//PI to the power x 
+		//PI to the power x
 		System.out.println("----Problem 1 - 1-----");
 		int x = RandomNumbers.getRandomInt(0, 9);
-		System.out.println("hi");
 		System.out.println(Math.pow(Math.PI, x));
 		
 		//y to the power PI
