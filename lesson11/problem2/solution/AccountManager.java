a7d7697e6095750f0b879d8bcfbb518158124d78
geetaputtappanavar <31633888+geetaputtappanavar@users.noreply.github.com>
2017-09-23 10:17:20 -0500

    Update AccountManager.java

--- lesson11/problem2/solution/AccountManager.java
+++ lesson11/problem2/solution/AccountManager.java
@@ -6,24 +6,7 @@ import java.util.List;
 
 public class AccountManager {
 	public static double computeAccountBalanceSum(List<Employee> emps) {
-		/*double sum = 0;
-		for(Employee e : emps) {
-			List accounts = e.getAccounts();
-			for(int i = 0; i < accounts.size(); ++i) {
-				Object next = accounts.get(i);
-				if(next instanceof SavingsAccount) {
-					sum += ((SavingsAccount)next).getBalance();
-				}
-				else if(next instanceof CheckingAccount) {
-					sum += ((CheckingAccount)next).getBalance();
-				}
-				else if(next instanceof RetirementAccount) {
-					sum += ((RetirementAccount)next).getBalance();
-				}
-			}
-		}
-		return sum;
-	*/ double sum = 0;
+	        double sum = 0;
 		for(Employee e:emps){
 			for(Account a:e.getAccounts()){
 				sum += a.getBalance();
